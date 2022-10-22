eb388c8a430146edf4b8b36d4ed59702647e0ea4
adi <adin72978@gmail.com>
2022-10-22 15:37:05 +0700

    fixing akun detail di menu

--- resources/views/akun_detail/create.blade.php
+++ resources/views/akun_detail/create.blade.php
@@ -53,6 +53,12 @@
                 </div>
             </div>
             <div class='form-group'>
+                <label for='' class="col-md-12">Quantity</label>
+                <div class="col-md-12">
+                    <input type='text' class='form-control' name='qty' id='' placeholder='Code'>
+                </div>
+            </div>
+            <div class='form-group'>
                 <label for='' class="col-md-12">Price</label>
                 <div class="col-md-12">
                     <input type='text' class='form-control' name='price' id='' placeholder='Code'>
